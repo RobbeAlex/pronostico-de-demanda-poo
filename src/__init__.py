71cb316da35116079d2bb450f357172904a29ed4
Roberto Alejandro Chagra Martinez <roberto.chagra4712@alumnos.udg.mx>
2025-12-01 08:21:22 -0600

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .main import MotorPronostico, Configuracion, VisualizadorPronosticos
+
+# Esto permite que alguien haga:
+# from src import MotorPronostico
+# en lugar de:
+# from src.main import MotorPronostico
